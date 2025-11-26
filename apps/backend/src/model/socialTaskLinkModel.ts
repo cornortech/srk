9535889d8df46c89be6fb259685765cf5cef9dee
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-11-26 12:37:45 +0545

    Updated Task Models

---
+++ apps/backend/src/model/socialTaskLinkModel.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const socialTaskLinkSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            unique: true,
+        },
+
+        facebookUrl: {
+            type: String,
+            default: ""
+        },
+        instagramUrl: {
+            type: String,
+            default: ""
+        },
+        tiktokUrl: {
+            type: String,
+            default: ""
+        },
+        youtubeUrl: {
+            type: String,
+            default: ""
+        },
+    },
+    { timestamps: true }
+);
+
+export const SocialTaskLinkModel = mongoose.model("SocialTaskLink", socialTaskLinkSchema);

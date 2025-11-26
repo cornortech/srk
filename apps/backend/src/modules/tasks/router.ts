90d7a077935f873b43125b4f4a062fd0453e40a8
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-11-26 15:14:16 +0545

    Create, Get, and Enroll Social Task Package

---
+++ apps/backend/src/modules/tasks/router.ts
@@ -0,0 +1,11 @@
+import { initServer } from "@ts-rest/express";
+import { taskContract } from "../../contract/task/contract";
+import { taskQueryHandler } from "./query";
+import { taskMutationHandler } from "./mutation";
+const s = initServer();
+
+export const taskRouter = s.router(taskContract, {
+ createSocialTaskPackage: taskMutationHandler.createSocialTaskPackage,
+ getSocialTaskPackage: taskQueryHandler.getSocialTaskPackage,
+ enrollSocialTaskPackage: taskMutationHandler.enrollSocialTaskPackage
+});

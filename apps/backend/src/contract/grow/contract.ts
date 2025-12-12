0fe839f77c55fb999ce2635ff35e70e2e099c39c
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-12 19:07:04 +0545

    Approve Social Grow Follow Request by Id

--- apps/backend/src/contract/grow/contract.ts
+++ apps/backend/src/contract/grow/contract.ts
@@ -1,7 +1,7 @@
 import { initContract } from "@ts-rest/core";
 import { createGrowSocialMediaEnrollementSchema } from "./schema";
 import { ErrorSchema, SuccessSchema } from "../common";
-import { error } from "console";
+import { z } from "zod";
 
 const c = initContract();
 
@@ -16,5 +16,18 @@ export const growContract = c.router({
             500: ErrorSchema,
         },
         summary: "Create a new grow social media enrollement",
+    },
+
+    acceptSocialGrowFollowRequest: {
+        method: "PATCH",
+        path: "/accept-social-grow-follow-request/:id",
+        body: z.object({}),
+        responses: {
+            200: SuccessSchema,
+            403: ErrorSchema,
+            404: ErrorSchema,
+            500: ErrorSchema
+        },
+        summary: "Approve Social Grow Follow Request by Id"
     },
 });
--- apps/backend/src/modules/grow/muation.ts
+++ apps/backend/src/modules/grow/muation.ts
@@ -1,10 +1,12 @@
-// import { AppRouteImplementation } from "@ts-rest/express";
+import { AppRouteImplementationOrOptions } from "@ts-rest/express/src/lib/types"
+import { growContract } from "../../contract/grow/contract"
+import { growSocialMediaPackageUserModel } from "../../model/growSocialMediaPackageUserModel"
 
 // const createGrowSocialMediaEnrollement:AppRouteImplementation<
 // typeof growContract.createGrowSocialMediaEnrollement
 // > = async ({ req, body }) => {
 //     try {
-        
+
 //         const {
 //             fullName,
 //             email,
@@ -26,6 +28,44 @@
 //     }
 // }
 
-// export const growMutationHandler = {
-//     createGrowSocialMediaEnrollement,
-// }
+const acceptSocialGrowFollowRequest: AppRouteImplementationOrOptions<typeof growContract.acceptSocialGrowFollowRequest> = async ({ params }) => {
+    try {
+        const enrollmentRequest = await growSocialMediaPackageUserModel.findOne({ _id: params.id })
+
+        if (!enrollmentRequest) {
+            return {
+                status: 500,
+                body: {
+                    message: "No Such Enrollment Found"
+                }
+            }
+        }
+
+        await growSocialMediaPackageUserModel.findOneAndUpdate(
+            { _id: params.id },
+            { $set: { status: "portalActivated" } },
+            { new: true }
+        )
+
+        return {
+            status: 200,
+            body: {
+                message: "Follow Request Approved",
+                success: true
+            }
+        }
+
+    } catch (error) {
+        console.log(error);
+        return ({
+            status: 500,
+            body: {
+                message: error.message ? `Internal server error: ${error.message}` : "Internal server error",
+                success: false
+            }
+        })
+    }
+}
+
+
+export const growMutationHandler = { acceptSocialGrowFollowRequest }

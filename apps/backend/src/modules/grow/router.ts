88685575799525083ac45fc5cc6d0cf45ec71ca8
Project-User0 <myproject.gk01@gmail.com>
2025-12-16 15:10:13 +0545

    Get all enrolled user, approve and reject api integration in the forntend

--- apps/backend/src/modules/grow/router.ts
+++ apps/backend/src/modules/grow/router.ts
@@ -1,11 +1,14 @@
 import { initServer } from "@ts-rest/express";
 import { growContract } from "@srk/shared/contracts";
 import { growMutationHandler } from "./mutation";
+import { growEnrollmentUserQueryHandler } from "./query";
 
 const s = initServer();
 
 export const growRouter = s.router(growContract, {
     createGrowSocialMediaEnrollement: growMutationHandler.createGrowSocialMediaEnrollement,
+    getAllGrowSocialMediaEnrollement: growEnrollmentUserQueryHandler.getAllSrkGrowEnrollementUser,
+    getGrowSocialMediaEnrollementById: growEnrollmentUserQueryHandler.getSrkGrowEnrollementUserById,
     validateGrowUserPromoCode: growMutationHandler.validateGrowUserPromoCode,
     acceptSocialGrowEnrollementRequest: growMutationHandler.acceptSocialGrowEnrollementRequest,
     rejectSocialGrowEnrollementRequest: growMutationHandler.rejectSocialGrowEnrollementRequest,
--- libs/shared/contracts/src/lib/grow/contract.ts
+++ libs/shared/contracts/src/lib/grow/contract.ts
@@ -1,62 +1,94 @@
-import { initContract } from "@ts-rest/core";
-import { createGrowSocialMediaEnrollementSchema, validateGrowUserPromoCodeResponseSchema, validateGrowUserPromoCodeSchema } from "./schema";
-import { ErrorSchema, SuccessSchema } from "../common";
-import { z } from "zod";
+import { initContract } from '@ts-rest/core';
+import {
+  createGrowSocialMediaEnrollementSchema,
+  getAllGrowSocialMediaEnrollementSchema,
+  getGrowSocialMediaEnrollementByIdSchema,
+  validateGrowUserPromoCodeResponseSchema,
+  validateGrowUserPromoCodeSchema,
+} from './schema';
+import { ErrorSchema, SuccessSchema } from '../common';
+import { z } from 'zod';
 
 const c = initContract();
 
 export const growContract = c.router({
-    createGrowSocialMediaEnrollement: {
-        method: "POST",
-        path: "/social-media-enrollement",
-        body: createGrowSocialMediaEnrollementSchema,
-        responses: {
-            201: SuccessSchema,
-            400: ErrorSchema,
-            409: ErrorSchema,
-            500: ErrorSchema,
-        },
-        summary: "Create a new grow social media enrollement for user with user details and payment details",
+  createGrowSocialMediaEnrollement: {
+    method: 'POST',
+    path: '/social-media-enrollement',
+    body: createGrowSocialMediaEnrollementSchema,
+    responses: {
+      201: SuccessSchema,
+      400: ErrorSchema,
+      409: ErrorSchema,
+      500: ErrorSchema,
     },
-
-    validateGrowUserPromoCode: {
-        method: "POST",
-        path: "/validate-promo-code",
-        body: validateGrowUserPromoCodeSchema,
-        responses: {
-            200: validateGrowUserPromoCodeResponseSchema,
-            400: ErrorSchema,
-            409: ErrorSchema,
-            500: ErrorSchema,
-        },
-        summary: "Validate enetered promo code and return its details with discount",
+    summary:
+      'Create a new grow social media enrollement for user with user details and payment details',
+  },
+  getAllGrowSocialMediaEnrollement: {
+    method: 'GET',
+    path: '/get-social-media-enrollement',
+    responses: {
+      200: getAllGrowSocialMediaEnrollementSchema,
+      400: ErrorSchema,
+      409: ErrorSchema,
+      500: ErrorSchema,
     },
-
-    acceptSocialGrowEnrollementRequest: {
-        method: "PUT",
-        path: "/grow/accept-social-grow-enrollement-request/:enrollementId",
-        body: z.object({}),
-        responses: {
-            200: SuccessSchema,
-            403: ErrorSchema,
-            404: ErrorSchema,
-            500: ErrorSchema
-        },
-        summary: "Approve Social Grow Enrollement Request by Id"
+    summary:
+      'Get all grow social media enrollement for user with user details and payment details',
+  },
+  getGrowSocialMediaEnrollementById: {
+    method: 'GET',
+    path: '/get-social-media-enrollement-by-id/:enrollmentID',
+    responses: {
+      200: getGrowSocialMediaEnrollementByIdSchema,
+      400: ErrorSchema,
+      409: ErrorSchema,
+      500: ErrorSchema,
     },
+    summary:
+      'Get grow social media enrollement for user by ID with user details and payment details',
+  },
 
-    rejectSocialGrowEnrollementRequest: {
-        method: "PUT",
-        path: "/grow/reject-social-grow-enrollement-request/:enrollementId",
-        body: z.object({
-            rejectionReason: z.string()
-        }),
-        responses: {
-            200: SuccessSchema,
-            403: ErrorSchema,
-            404: ErrorSchema,
-            500: ErrorSchema
-        },
-        summary: "Reject Social Grow Enrollement Request by Id"
-    }
+  validateGrowUserPromoCode: {
+    method: 'POST',
+    path: '/validate-promo-code',
+    body: validateGrowUserPromoCodeSchema,
+    responses: {
+      200: validateGrowUserPromoCodeResponseSchema,
+      400: ErrorSchema,
+      409: ErrorSchema,
+      500: ErrorSchema,
+    },
+    summary:
+      'Validate enetered promo code and return its details with discount',
+  },
+
+  acceptSocialGrowEnrollementRequest: {
+    method: 'PUT',
+    path: '/grow/accept-social-grow-enrollement-request/:enrollementId',
+    body: z.object({}),
+    responses: {
+      200: SuccessSchema,
+      403: ErrorSchema,
+      404: ErrorSchema,
+      500: ErrorSchema,
+    },
+    summary: 'Approve Social Grow Enrollement Request by Id',
+  },
+
+  rejectSocialGrowEnrollementRequest: {
+    method: 'PUT',
+    path: '/grow/reject-social-grow-enrollement-request/:enrollementId',
+    body: z.object({
+      rejectionReason: z.string(),
+    }),
+    responses: {
+      200: SuccessSchema,
+      403: ErrorSchema,
+      404: ErrorSchema,
+      500: ErrorSchema,
+    },
+    summary: 'Reject Social Grow Enrollement Request by Id',
+  },
 });

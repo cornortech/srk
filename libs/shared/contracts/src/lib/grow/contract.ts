07353757c9119f5b49c2c334f84ae781dd4b2cf7
Chandan <chandangautam322@gmail.com>
2025-12-17 05:28:26 +0545

    feat: add functionality to retrieve all SRK Grow users with detailed information

--- libs/shared/contracts/src/lib/grow/contract.ts
+++ libs/shared/contracts/src/lib/grow/contract.ts
@@ -1,10 +1,11 @@
 import { initContract } from '@ts-rest/core';
 import {
-  createGrowSocialMediaEnrollementSchema,
-  getAllGrowSocialMediaEnrollementSchema,
-  getGrowSocialMediaEnrollementByIdSchema,
-  validateGrowUserPromoCodeResponseSchema,
-  validateGrowUserPromoCodeSchema,
+    createGrowSocialMediaEnrollementSchema,
+    getAllSrkGrowUsersResponseSchema,
+    getAllGrowSocialMediaEnrollementSchema,
+    getGrowSocialMediaEnrollementByIdSchema,
+    validateGrowUserPromoCodeResponseSchema,
+    validateGrowUserPromoCodeSchema,
 } from './schema';
 import { ErrorSchema, SuccessSchema } from '../common';
 import { z } from 'zod';
@@ -12,83 +13,95 @@ import { z } from 'zod';
 const c = initContract();
 
 export const growContract = c.router({
-  createGrowSocialMediaEnrollement: {
-    method: 'POST',
-    path: '/social-media-enrollement',
-    body: createGrowSocialMediaEnrollementSchema,
-    responses: {
-      201: SuccessSchema,
-      400: ErrorSchema,
-      409: ErrorSchema,
-      500: ErrorSchema,
-    },
-    summary:
-      'Create a new grow social media enrollement for user with user details and payment details',
-  },
-  getAllGrowSocialMediaEnrollement: {
-    method: 'GET',
-    path: '/get-social-media-enrollement',
-    responses: {
-      200: getAllGrowSocialMediaEnrollementSchema,
-      400: ErrorSchema,
-      409: ErrorSchema,
-      500: ErrorSchema,
-    },
-    summary:
-      'Get all grow social media enrollement for user with user details and payment details',
-  },
-  getGrowSocialMediaEnrollementById: {
-    method: 'GET',
-    path: '/get-social-media-enrollement-by-id/:enrollmentID',
-    responses: {
-      200: getGrowSocialMediaEnrollementByIdSchema,
-      400: ErrorSchema,
-      409: ErrorSchema,
-      500: ErrorSchema,
-    },
-    summary:
-      'Get grow social media enrollement for user by ID with user details and payment details',
-  },
 
-  validateGrowUserPromoCode: {
-    method: 'POST',
-    path: '/validate-promo-code',
-    body: validateGrowUserPromoCodeSchema,
-    responses: {
-      200: validateGrowUserPromoCodeResponseSchema,
-      400: ErrorSchema,
-      409: ErrorSchema,
-      500: ErrorSchema,
+    getAllGrowSocialMediaEnrollement: {
+        method: 'GET',
+        path: '/get-social-media-enrollement',
+        responses: {
+            200: getAllGrowSocialMediaEnrollementSchema,
+            400: ErrorSchema,
+            409: ErrorSchema,
+            500: ErrorSchema,
+        },
+        summary:
+            'Get all grow social media enrollement for user with user details and payment details',
     },
-    summary:
-      'Validate enetered promo code and return its details with discount',
-  },
 
-  acceptSocialGrowEnrollementRequest: {
-    method: 'PUT',
-    path: '/grow/accept-social-grow-enrollement-request/:enrollementId',
-    body: z.object({}),
-    responses: {
-      200: SuccessSchema,
-      403: ErrorSchema,
-      404: ErrorSchema,
-      500: ErrorSchema,
+    getGrowSocialMediaEnrollementById: {
+        method: 'GET',
+        path: '/get-social-media-enrollement-by-id/:enrollmentID',
+        responses: {
+            200: getGrowSocialMediaEnrollementByIdSchema,
+            400: ErrorSchema,
+            409: ErrorSchema,
+            500: ErrorSchema,
+        },
+        summary:
+            'Get grow social media enrollement for user by ID with user details and payment details',
     },
-    summary: 'Approve Social Grow Enrollement Request by Id',
-  },
 
-  rejectSocialGrowEnrollementRequest: {
-    method: 'PUT',
-    path: '/grow/reject-social-grow-enrollement-request/:enrollementId',
-    body: z.object({
-      rejectionReason: z.string(),
-    }),
-    responses: {
-      200: SuccessSchema,
-      403: ErrorSchema,
-      404: ErrorSchema,
-      500: ErrorSchema,
+    getAllSrkGrowUsers: {
+        method: "GET",
+        path: "/grow-users",
+        responses: {
+            200: getAllSrkGrowUsersResponseSchema,
+            500: ErrorSchema,
+        },
+        summary: "Get all srk grow users who is registered",
     },
-    summary: 'Reject Social Grow Enrollement Request by Id',
-  },
+
+    createGrowSocialMediaEnrollement: {
+        method: "POST",
+        path: "/social-media-enrollement",
+        body: createGrowSocialMediaEnrollementSchema,
+        responses: {
+            201: SuccessSchema,
+            400: ErrorSchema,
+            409: ErrorSchema,
+            500: ErrorSchema,
+        },
+        summary: "Create a new grow social media enrollement for user with user details and payment details",
+    },
+
+    validateGrowUserPromoCode: {
+        method: 'POST',
+        path: '/validate-promo-code',
+        body: validateGrowUserPromoCodeSchema,
+        responses: {
+            200: validateGrowUserPromoCodeResponseSchema,
+            400: ErrorSchema,
+            409: ErrorSchema,
+            500: ErrorSchema,
+        },
+        summary:
+            'Validate enetered promo code and return its details with discount',
+    },
+
+    acceptSocialGrowEnrollementRequest: {
+        method: 'PUT',
+        path: '/grow/accept-social-grow-enrollement-request/:enrollementId',
+        body: z.object({}),
+        responses: {
+            200: SuccessSchema,
+            403: ErrorSchema,
+            404: ErrorSchema,
+            500: ErrorSchema,
+        },
+        summary: 'Approve Social Grow Enrollement Request by Id',
+    },
+
+    rejectSocialGrowEnrollementRequest: {
+        method: 'PUT',
+        path: '/grow/reject-social-grow-enrollement-request/:enrollementId',
+        body: z.object({
+            rejectionReason: z.string(),
+        }),
+        responses: {
+            200: SuccessSchema,
+            403: ErrorSchema,
+            404: ErrorSchema,
+            500: ErrorSchema,
+        },
+        summary: 'Reject Social Grow Enrollement Request by Id',
+    },
 });

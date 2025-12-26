4cfd7955de02de39a772a9fb96fc0abb703ebe82
santosh kunwar <santoshkunwar@santoshs-MacBook-Air.local>
2025-12-26 16:56:26 +0545

    grow affiliate payout request and accept / reject api done

--- apps/backend/src/modules/grow/affiliate/router.ts
+++ apps/backend/src/modules/grow/affiliate/router.ts
@@ -1,11 +1,23 @@
 import { growAffiliateContract } from '@srk/shared/contracts';
 import { initServer } from '@ts-rest/express';
 import { growAffiliateQueryHandler } from './query';
+import { growAffiliateMutationHandler } from './mutation';
 
 const s = initServer();
 
 export const growAffiliateRouter = s.router(growAffiliateContract, {
-  getGrowAffiliateUserComissionEarningsDashboard: growAffiliateQueryHandler.getGrowAffiliateUserComissionEarningsDashboard,
-  getUserAffiliateSalesComissionEarnings: growAffiliateQueryHandler.getUserAffiliateSalesComissionEarnings,
-  getAllUsersAffiliateComissionLeaderBoard: growAffiliateQueryHandler.getAllUsersAffiliateComissionLeaderBoard,
+  getGrowAffiliateUserComissionEarningsDashboard:
+    growAffiliateQueryHandler.getGrowAffiliateUserComissionEarningsDashboard,
+  getUserAffiliateSalesComissionEarnings:
+    growAffiliateQueryHandler.getUserAffiliateSalesComissionEarnings,
+  getAllUsersAffiliateComissionLeaderBoard:
+    growAffiliateQueryHandler.getAllUsersAffiliateComissionLeaderBoard,
+  acceptSrkAffiliateEarningPayout:
+    growAffiliateMutationHandler.acceptSrkAffiliateEarningPayout,
+  createSrkAffiliateEarningPayout:
+    growAffiliateMutationHandler.createSrkAffiliateEarningPayout,
+  rejectSrkAffiliateEarningPayout:
+    growAffiliateMutationHandler.rejectSrkAffiliateEarningPayout,
+  getSrkAffiliateEarningPayoutForAdmin:
+    growAffiliateQueryHandler.getAllSrkAffiliateEarningPayoutForAdmin,
 });
